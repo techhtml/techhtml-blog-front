4bbf1b523a5d3518c9f06a23a555a7021f5d2877
techhtml <apes0123@gmail.com>
2022-07-28 22:20:41 +0900

    파일 업로드 구현

--- pages/posts/add.tsx
+++ pages/posts/add.tsx
@@ -1,9 +1,10 @@
-import {ChangeEventHandler, FormEventHandler, useState} from 'react';
+import {ChangeEventHandler, FormEventHandler, useEffect, useState} from 'react';
 import {API_HOST} from '../../common';
 import {useRouter} from 'next/router';
 import {marked} from 'marked';
 import styled from '@emotion/styled';
 import {NextPage} from 'next';
+import Head from 'next/head';
 
 const PostAddPage: NextPage = () => {
   const [title, setTitle] = useState<string>('');
@@ -16,6 +17,24 @@ const PostAddPage: NextPage = () => {
 
   const handleContent: ChangeEventHandler<HTMLTextAreaElement> = e => {
     setContent(e.target.value);
+  };
+
+  const handleFile: ChangeEventHandler<HTMLInputElement> = async e => {
+    const {files} = e.target;
+    if (!files) {
+      return;
+    }
+    for (const file of files) {
+      const result = await fetch(`${API_HOST}/upload`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/octet-stream',
+        },
+        body: file,
+      });
+      const text = await result.text();
+      console.log(text);
+    }
   };
 
   const handleSubmit: FormEventHandler = async e => {
@@ -37,26 +56,41 @@ const PostAddPage: NextPage = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="title">제목</label>
-        <input type="text" id="title" value={title} onChange={handleTitle} />
-      </div>
-      <div>
-        <label htmlFor="content">본문</label>
-        <Container>
-          <textarea
-            id="content"
-            cols={30}
-            rows={10}
-            onChange={handleContent}
-            defaultValue={content}
+    <>
+      <Head>
+        <title>게시글 작성하기</title>
+      </Head>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">제목</label>
+          <input type="text" id="title" value={title} onChange={handleTitle} />
+        </div>
+        <div>
+          <label htmlFor="content">본문</label>
+          <Container>
+            <textarea
+              id="content"
+              cols={30}
+              rows={10}
+              onChange={handleContent}
+              defaultValue={content}
+            />
+            <div dangerouslySetInnerHTML={{__html: marked.parse(content)}} />
+          </Container>
+        </div>
+        <div>
+          <label htmlFor="file">제목</label>
+          <input
+            id="file"
+            type="file"
+            onChange={handleFile}
+            multiple
+            accept=".jpg, .jpeg, .png"
           />
-          <div dangerouslySetInnerHTML={{__html: marked.parse(content)}} />
-        </Container>
-      </div>
-      <button>게시글 작성</button>
-    </form>
+        </div>
+        <button>게시글 작성</button>
+      </form>
+    </>
   );
 };
 
